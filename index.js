44429cf083a2f864696a315721c54f6f28975661
Alberth Mayta Cuevas <alberth.mayta@tecsup.edu.pe>
2025-05-05 09:46:46 -0500

    Corregir respuesta HTTP para Render

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
-const http= require('http')
+const http = require('http')
 
-function requestController(){
+function requestController(req, res) {
+    res.end('Bienvenidos al curso') // Esto es importante para Render
     console.log('Bienvenidos al curso')
 }
 
-const server=http.createServer(requestController)
+const server = http.createServer(requestController)
 
-const PORT=process.env.PORT
+const PORT = process.env.PORT || 3000
 
-server.listen(PORT, function(){
-    console.log("Aplicacion corriendo en: " + PORT)
+server.listen(PORT, () => {
+    console.log("Aplicación corriendo en: " + PORT)
 })
